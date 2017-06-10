aa8441aac7db80461cea1b4bebb0ed824a60ab8c
Rasmus Tilljander <tilljander.rasmus@gmail.com>
2017-06-10 22:58:03 +0200

    Added Weapon Collision and Health for the projectile

---
+++ Assets/Scripts/Collision/WeaponCollision.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Health))]
+public class WeaponCollision : MonoBehaviour
+{
+    public int damage;
+
+    private Health health;
+
+    void Start()
+    {
+        health = GetComponent<Health>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Health collisionHealth = collision.gameObject.GetComponent<Health>();
+        collisionHealth.Damage(damage);
+        health.Damage(1);
+    }
+}
