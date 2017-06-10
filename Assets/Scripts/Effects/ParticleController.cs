29278ee68f2b100835446f9df33368efaf003fb4
narkos <narkos.newb@gmail.com>
2017-06-10 23:51:07 +0200

    thrusters and some post processing

---
+++ Assets/Scripts/Effects/ParticleController.cs
@@ -0,0 +1,35 @@
+﻿using InControl;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleController : MonoBehaviour {
+	public KeybindingManager keybindingManager;
+	protected AvailablePlayerActions availablePlayerActions;
+	public GameObject thrustersHolder;
+	private List<ParticleSystem> thrusterSystems;
+
+	// Use this for initialization
+	void Start () {
+		thrusterSystems = new List<ParticleSystem>();
+		gameObject.GetComponentsInChildren<ParticleSystem>(true, thrusterSystems);
+
+		availablePlayerActions = keybindingManager.GetCurrentAvailablePlayerActions();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate() {
+		if(availablePlayerActions.forward.State) 
+		{
+			for(int i = 0; i < thrusterSystems.Count; i++) 
+			{
+				thrusterSystems[i].Emit(2);
+			}
+		}
+	}
+}
