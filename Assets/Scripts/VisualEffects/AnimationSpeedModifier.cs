2016d39467cff734e14a95db69449b8622740598
Fetdressing <simon.home@hotmail.com>
2017-06-18 15:50:42 +0200

    test med tentakler

---
+++ Assets/Scripts/VisualEffects/AnimationSpeedModifier.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationSpeedModifier : MonoBehaviour {
+    public float speedIncrease = 1.5f;
+    // Use this for initialization
+    void Start()
+    {
+        foreach (AnimationState state in GetComponent<Animation>())
+        {
+            state.speed = speedIncrease;
+        }
+
+    }
+
+    void Awake()
+    {
+        foreach (AnimationState state in GetComponent<Animation>())
+        {
+            state.speed = speedIncrease;
+        }
+
+    }
+
+}
